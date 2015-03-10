24f44ff13bb49192f85e1de7067ca8dbbfe42f76
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2015-03-10 16:53:56 +0100

    example without recursion

--- example/ndfs.c
+++ example/ndfs.c
@@ -35,6 +35,7 @@ node_t               s0;
 
 typedef struct stack_elem_s stack_elem_t;
 struct stack_elem_s {
+    node_t         *s;
     node_t        **succs;
     size_t          count;
     size_t          index;
@@ -42,7 +43,7 @@ struct stack_elem_s {
 };
 
 void
-init_node (node_t *s)
+init_node (node_t *s, node_t *prev, int index)
 {
     s->accepting = __VERIFIER_nondet_uint() % 2;
     s->red = __VERIFIER_nondet_uint() % 2;
@@ -51,6 +52,7 @@ init_node (node_t *s)
         s->pink[i] = __VERIFIER_nondet_uint() % 2;
         s->color[i] = __VERIFIER_nondet_uint() % 3;
     }
+    (void) prev;
 }
 
 static inline void
@@ -61,15 +63,16 @@ delete_successors (stack_elem_t *stack)
 }
 
 static inline stack_elem_t *
-create_successors (stack_elem_t *prev)
+create_successors (stack_elem_t *prev, node_t *s)
 {
     stack_elem_t *elem = malloc (sizeof (stack_elem_t));
     elem->count = __VERIFIER_nondet_uint() % MAX;
     elem->succs = malloc (sizeof (node_t[elem->count]));
     elem->index = 0;
+    elem->s = s;
     elem->prev = prev;
     for (int i = 0; i < elem->count; i++) {
-        init_node (elem->succs[i]);
+        init_node (elem->succs[i], s, i);
     }
     return elem;
 }
@@ -103,52 +106,62 @@ all_blue_cyan_or_red (stack_elem_t *next, int p)
 }
 
 void
-dfsred (node_t *s, int p, stack_elem_t *stack)
+dfsred (int p, stack_elem_t *stack)
 {
-    stack_elem_t *next = create_successors (stack);
+    stack_elem_t *seed = stack;
+    //assert (all_blue_cyan_or_red(stack, p)); // TODO: pink_p \subseteq blue \cup cyan_p
+    stack->s->pink[p] = true;
+    while (stack) {
+        node_t *s = stack->s;
+        if (stack->index != stack->count) {
+            node_t *t = stack->succs[stack->count++]; // popSuccessor
+            if (t->color[p] == cyan) exit(1);
+            if (!t->pink[p] && !t->red) {
+                //assert (all_blue_cyan_or_red(stack, p)); // TODO: pink_p \subseteq blue \cup cyan_p
+                t->pink[p] = true;
+                stack = create_successors (stack, t);
+            }
+        } else { // backtrack:
+            if (s->accepting) {
+                __sync_fetch_and_add (&s->count, -1); // count -= 1
+                while (*((volatile size_t *)&s->count) > 0) {}
+            }
+            assert (all_pink_or_red_not_cyan(stack, p));
+            s->red = true;
+            s->pink[p] = false;
 
-    assert (all_blue_cyan_or_red(next, p));
-    s->pink[p] = true;
-    for (int i = 0; i < next->count; i++) {
-        node_t *t = next->succs[i];
-        if (t->color[p] == cyan) exit(1);
-        if (!s->pink[p] && !t->red) {
-            dfsred(t, p, next);
+            if (stack == seed) return; // dfsblue does cleanup
+            stack_elem_t *stackp = stack;
+            stack = stack->prev;
+            delete_successors(stackp);
         }
     }
-
-    if (s->accepting) {
-        __sync_fetch_and_add (&s->count, -1); // count -= 1
-        while (*((volatile size_t *)&s->count) > 0) {}
-    }
-
-    assert (all_pink_or_red_not_cyan(next, p));
-    s->red = true;
-    s->pink[p] = false;
-
-    delete_successors (next);
 }
 
 void
-dfsblue (node_t *s, int p, stack_elem_t *stack)
+dfsblue (int p, stack_elem_t *stack)
 {
-    stack_elem_t *next = create_successors (stack);
-
-    s->color[p] = cyan;
-    for (int i = 0; i < next->count; i++) {
-        node_t *t = next->succs[i];
-        if (t->color[p] == white && !t->red) {
-            dfsblue (t, p, next);
+    stack->s->color[p] = cyan;
+    while (stack) {
+        node_t *s = stack->s;
+        if (stack->index != stack->count) {
+            node_t *t = stack->succs[stack->count++]; // popSuccessor
+            if (t->color[p] == white && !t->red) {
+                t->color[p] = cyan;
+                stack = create_successors (stack, t);
+            }
+        } else { // backtrack:
+            assert (all_blue_cyan_or_red(stack, p));
+            if (s->accepting) {
+                __sync_fetch_and_add (&s->count, 1); // count += 1
+                dfsred (p, stack);
+            }
+            s->color[p] = blue;
+            stack_elem_t *stackp = stack;
+            stack = stack->prev;
+            delete_successors(stackp);
         }
     }
-    if (s->accepting) {
-        __sync_fetch_and_add (&s->count, 1); // count += 1
-        dfsred (s, p, stack);
-    }
-
-    assert (all_blue_cyan_or_red(next, p));
-    s->color[p] = blue;
-    delete_successors (next);
 }
 
 void *
@@ -156,20 +169,21 @@ ndfs (void *x)
 {
     int t = (size_t) x;
 
-    dfsblue (&s0, t, NULL);
+    stack_elem_t *stack = create_successors (stack, &s0);
+    dfsblue (t, stack);
     return NULL;
 }
 
 int
 main ()
 {
-    init_node (&s0);
+    init_node (&s0, NULL, 0);
 
     pthread_t threads[T];
     for (size_t i = 0; i < T; i++) {
         pthread_create (&threads[i], NULL, ndfs, (void *)i);
     }
-    for (int i = 0; i < T; i++) {
+    for (size_t i = 0; i < T; i++) {
         pthread_join (threads[i], NULL);
     }
     return 0;
