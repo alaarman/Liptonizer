5920b7a8c2ba2858dce0e204783a73e777d8124a
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2015-03-16 23:58:34 +0100

    Util

---
+++ LiptonBin/llvm/Util.h
@@ -0,0 +1,32 @@
+/*
+ * Util.h
+ *
+ *  Created on: Mar 13, 2015
+ *      Author: laarman
+ */
+
+#ifndef LIPTONBIN_LLVM_UTIL_H_
+#define LIPTONBIN_LLVM_UTIL_H_
+
+#include <iostream>
+#include <string>
+#include <llvm/Support/raw_ostream.h>
+
+using namespace std;
+
+#define endll "\n"
+
+#ifndef NDEBUG
+#   define LLASSERT(condition, message) \
+    do { \
+        if (! (condition)) { \
+            outs() << "Assertion `" #condition "` failed in " << __FILE__ \
+                 << " line " << __LINE__ << ": " << message << "\n"; \
+            exit(EXIT_FAILURE); \
+        } \
+    } while (false)
+#else
+#   define LLASSERT(condition, message) do { } while (false)
+#endif
+
+#endif /* LIPTONBIN_LLVM_UTIL_H_ */
