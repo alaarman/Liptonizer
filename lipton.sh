9170149123101ac28a23930de7249ce285d3cb87
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2015-09-03 02:40:08 +0200

    Support inlining all functions

--- lipton.sh
+++ lipton.sh
@@ -7,24 +7,15 @@ BC=${BC/%cpp/bc}
 
 set -e
 
-clang -emit-llvm -c $1 -o $BC
+clang -O0 -Iinclude -emit-llvm -c $1 -o $BC
 
 llvm-dis $BC
 
 OPT=${BC/%\.bc/-opt.bc}
 
-opt -mem2reg \
-    -internalize-public-api-list=main \
-    -inline \
-    -loops \
-    -loop-simplify \
-    -loop-rotate \
-    -lcssa \
-    -loop-unroll \
-    -instnamer \
-    -dce \
+opt -mem2reg -internalize-public-api-list=main -internalize -inline -loops \
+    -loop-simplify -loop-rotate -lcssa -loop-unroll \
     $BC > $OPT
-
 
 llvm-dis $OPT
     
