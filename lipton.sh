298fe966a1732d7c37b93e74ddaa9ca29511594a
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2015-10-30 03:32:18 +0100

    debug in script

--- lipton.sh
+++ lipton.sh
@@ -14,23 +14,27 @@ mkfifo $PIPE
 exec 3<>$PIPE
 rm $PIPE
 
+DEBUG=0
 for a in `seq 2 $#`; do
-	XX="${@:${a}}"
+	XX="${!a}"
 	if [ "$XX" == "-d" ]; then
-		sed "s/<pthread.h>/<\/usr\/bin\/pthread.h>/g"  $1 >3
+		sed "s/<pthread.h>/<\/usr\/include\/pthread.h>/g" $1 | \
+			sed "s/<assert.h>/<\/usr\/include\/assert.h>/g" >3
+		DEBUG=1
+		echo "DEBUG MODE!!!!!!!!!!!!!!!!!!!!!"
 		break
 	fi
 done
 
-if [ -z $CCODE ]; then
+if [ $DEBUG = 0 ]; then
+	echo "Normal mode"
 	cat $1 >3
 fi
 
-#echo <3
 
 #echo "$CCODE" >3
 
-clang -O0 -Iinclude -emit-llvm -Iic3-haskell/include/  -o $BC -c -x c - <3 #-O1 for TBAA
+clang -O0 -Iinclude -fno-rtti -emit-llvm -Iic3-haskell/include/  -o $BC -c -x c - <3 #-O1 for TBAA
 
 #clang -O1 -Iinclude -emit-llvm -Iic3-haskell/include/ -c $1 -o $BC #-O1 for TBAA
 
@@ -43,6 +47,8 @@ opt -mem2reg -internalize-public-api-list=main -internalize -inline -loops \
     $BC > $OPT
 
 llvm-dis $OPT
+
+echo $OPT
     
 F=${OPT/%\.bc/-lipton.bc}
 
