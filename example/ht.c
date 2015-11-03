795c423bde2a4f84a6ad4c5b8f185e88169fd315
Alfons Laarman <a.w.laarman@ewi.utwente.nl>
2015-11-03 20:37:57 +0100

    5 process hash table example

--- example/ht.c
+++ example/ht.c
@@ -23,6 +23,7 @@ int
 find_or_insert (int val)
 {
 	int h = val % N;
+
 	int index = h;
 	for (int i = 0; i < PROBE_MAX; ++i) {
 #ifdef DEBUG
@@ -45,10 +46,10 @@ find_or_insert (int val)
 }
 
 void *
-process (void *arg)
+process1 (void *arg)
 {
   //pthread_join(ht_init,0);
-  int val = *(int *)arg;
+  int val = 1;//(int )arg;
   int found = find_or_insert (val);
   assert (found);// != FULL);
 #ifdef DEBUG
@@ -57,6 +58,70 @@ process (void *arg)
   return NULL;//(void *) (long) found; // pthread_exit( (void *) found )
 }
 
+void *
+process2 (void *arg)
+{
+  //pthread_join(ht_init,0);
+  int val = 59;//(int )arg;
+  int found = find_or_insert (val);
+  assert (found);// != FULL);
+#ifdef DEBUG
+  printf ("%d = %s\n", val, found ? "found" : "inserted");
+#endif
+  return NULL;//(void *) (long) found; // pthread_exit( (void *) found )
+}
+
+void *
+process3 (void *arg)
+{
+  //pthread_join(ht_init,0);
+  int val = 9;//(int )arg;
+  int found = find_or_insert (val);
+  assert (found);// != FULL);
+#ifdef DEBUG
+  printf ("%d = %s\n", val, found ? "found" : "inserted");
+#endif
+  return NULL;//(void *) (long) found; // pthread_exit( (void *) found )
+}
+
+void *
+process4 (void *arg)
+{
+  //pthread_join(ht_init,0);
+  int val = 41;//(int )arg;
+  int found = find_or_insert (val);
+  assert (found);// != FULL);
+#ifdef DEBUG
+  printf ("%d = %s\n", val, found ? "found" : "inserted");
+#endif
+  return NULL;//(void *) (long) found; // pthread_exit( (void *) found )
+}
+
+void *
+process5 (void *arg)
+{
+  //pthread_join(ht_init,0);
+  int val = 7;//(int )arg;
+  int found = find_or_insert (val);
+  assert (found);// != FULL);
+#ifdef DEBUG
+  printf ("%d = %s\n", val, found ? "found" : "inserted");
+#endif
+  return NULL;//(void *) (long) found; // pthread_exit( (void *) found )
+}
+
+void *
+process6 (void *arg)
+{
+  //pthread_join(ht_init,0);
+  int val = 55;//(int )arg;
+  int found = find_or_insert (val);
+  assert (found);// != FULL);
+#ifdef DEBUG
+  printf ("%d = %s\n", val, found ? "found" : "inserted");
+#endif
+  return NULL;//(void *) (long) found; // pthread_exit( (void *) found )
+}
 
 #define T 2				// threads
 int value[T] = {  1, 59, };//9, };// 52, 41  };
@@ -71,17 +136,39 @@ main ()
 //    find_or_insert (7);
 //    find_or_insert (9);
 //    find_or_insert (59);
+    int v1 = 1;
+    int v2 = 59;
 
-    pthread_t t[T];
-	for (int i = 0; i < T; i++)
-		pthread_create (t + i, NULL, process, (void *) &value[i]);
+    pthread_t t1;
+    pthread_t t2;
+    pthread_t t3;
+    pthread_t t4;
+    pthread_t t5;
+    pthread_t t6;
+	//for (int i = 0; i < T; i++)
+		pthread_create (&t1, NULL, process1, NULL);//(void *) &value[i]);
+		pthread_create (&t2, NULL, process2, NULL);//(void *) &value[i]);
+        pthread_create (&t3, NULL, process3, NULL);//(void *) &value[i]);
+        pthread_create (&t4, NULL, process4, NULL);//(void *) &value[i]);
+        pthread_create (&t5, NULL, process5, NULL);//(void *) &value[i]);
+        pthread_create (&t6, NULL, process6, NULL);//(void *) &value[i]);
 
 	int found_total = 0;
 	//long res;
-	for (int i = 0; i < T; i++) {
-		pthread_join (t[i], NULL);// (void **)&res);
+//	for (int i = 0; i < T; i++) {
+		pthread_join (t1, NULL);// (void **)&res);
 		found_total += 1;//res;
-	}
+		pthread_join (t2, NULL);// (void **)&res);
+		found_total += 1;//res;
+        pthread_join (t3, NULL);// (void **)&res);
+        found_total += 1;//res;
+        pthread_join (t4, NULL);// (void **)&res);
+        found_total += 1;//res;
+        pthread_join (t5, NULL);// (void **)&res);
+        found_total += 1;//res;
+        pthread_join (t6, NULL);// (void **)&res);
+        found_total += 1;//res;
+//	}
 	//assert (found_total == 4); // T - (unique values in value array)
 	return 0;
 }
